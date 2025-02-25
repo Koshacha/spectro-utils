644c283c09e262c0b0232b7aacbb9ca8183b2090
Ilya M. <wuvuxd@proton.me>
2025-02-25 23:14:34 +0700

    ref!: exec method

--- utils/Route.php
+++ utils/Route.php
@@ -2,134 +2,91 @@
 
 require_once __DIR__ . '/Template.php';
 require_once __DIR__ . '/Router.php';
+require_once __DIR__ . '/Inject.php';
 
 class Route {
     private static $routes = [];
     private static $template;
     public static $data = [];
 
-    public static function page($path, $callback, $staticMeta = []) {
+    private static function exec() {
         $uri = $_SERVER['REQUEST_URI'];
-        $pattern = self::pathToRegex($path);
-        self::setMeta($staticMeta);
-
         $uri = str_replace('frame/', '', $uri);
-    
-        if (preg_match($pattern, $uri, $matches)) {
-            array_shift($matches);
-    
-            if (is_string($callback) && strpos($callback, '::') !== false) {
-                list($class, $method) = explode('::', $callback, 2);
-                $reflection = new ReflectionMethod($class, $method);
-            } elseif (is_array($callback) && count($callback) === 2 && is_object($callback[0])) {
-                $reflection = new ReflectionMethod($callback[0], $callback[1]);
-            } elseif (is_string($callback) && function_exists($callback)) {
-                $reflection = new ReflectionFunction($callback);
-            } elseif (is_string($callback) && !function_exists($callback)) {
-                $GLOBALS['data'] = $callback;
-                $GLOBALS['dosomething'] = 1;
-                return;
-            } elseif (is_object($callback) && ($callback instanceof Closure)) {
-                $reflection = new ReflectionFunction($callback);
+
+        foreach (self::$routes as $path => $route) {
+            $is_bool = is_bool($path);
+            $is_current = false;
+
+            if ($is_bool) {
+                $is_current = $path;
+                $matches = [];
             } else {
-                trigger_error("Invalid callback format.", E_USER_WARNING);
-                return;
+                $pattern = self::pathToRegex($path);
+                $is_current = preg_match($pattern, $uri, $matches);
             }
-    
-            $parameters = $reflection->getParameters();
-            $args = [];
 
-            $req = Request::get();
-            $files = Request::files();
-    
-            foreach ($parameters as $parameter) {
-                $name = $parameter->getName();
-                $value = null;
-                $index = null;
-    
-                foreach ($matches as $key => $matchValue) {
-                  if($key === $name) {
-                    $value = $matchValue;
-                    $index = $key;
-                    break;
-                  }
+            if ($is_current) {
+                array_shift($matches);
+
+                self::$data = (new Inject($route['callback']))->setArguments($matches)->exec();
+
+                switch ($route['type']) {
+                    case 'json': {
+                        header('Content-Type: application/json');
+                        echo json_encode(self::$data);
+                        exit;
+                    }
+                    case 'lazy': {
+                        self::setMeta($route['staticMeta']);
+
+                        $frameUrl = $uri . '/frame/';
+                        $frameUrl = str_replace('//', '/', $frameUrl);
+
+                        $GLOBALS['data'] = '<div id="spectro_block5"><center><img src="{SITE}spectro-cms-loading.gif" height="50"></center><input id="spectro_dynamicblock5" type="hidden" value="' . $frameUrl .'"></div>';
+                        $GLOBALS['dosomething'] = 1;
+                        break;
+                    }
+                    case 'page':
+                    default: {
+                        self::setMeta($route['staticMeta']);
+
+                        if (self::$template) {
+                            self::renderTemplate(self::$template, self::$data);
+                        } else {
+                            self::renderTemplate($GLOBALS['data'], self::$data);
+                        }
+                    }
                 }
 
-                if ($name === 'request') {
-                    $value = $req;
-                } elseif ($name === 'files') {
-                    $value = $files;
-                } elseif (array_key_exists($name, $req)) {
-                    $value = $req[$name];
-                }
                 
-                if ($value === null && array_key_exists($name, $GLOBALS)) {
-                  $value = $GLOBALS[$name];
-                }
-    
-                if ($value === null) {
-                  if ($parameter->isOptional()) {
-                      try {
-                        $value = $parameter->getDefaultValue();
-                      } catch (ReflectionException $e) {
-                          
-                      }
-                  } else {
-                    trigger_error("Missing argument: " . $name, E_USER_WARNING);
-                    return;
-                  }
-                }
-    
-                if($index !== null) {
-                //   unset($matches[$index]);
-                }
-    
-                $args[] = $value;
-            }
-
-            self::$data = $reflection->invokeArgs($args);
-            // Для методов объекта:
-            // self::$data = $reflection->invokeArgs($callback[0], $args);
-
-            if (array_key_exists('json', $staticMeta) && $staticMeta['json']) {
-                header('Content-Type: application/json');
-                echo json_encode(self::$data);
-                die();
-            }
-    
-            if (self::$template) {
-                self::renderTemplate(self::$template, self::$data);
-            } else {
-                self::renderTemplate($GLOBALS['data'], self::$data);
             }
         }
+    }
+
+    public static function page($path, $callback, $staticMeta = []) {
+        self::$routes[$path] = [
+            'path' => $path,
+            'callback' => $callback,
+            'staticMeta' => $staticMeta,
+            'type' => 'page'
+        ];
     }
 
     public static function lazyPage($path, $callback, $staticMeta = []) {
-        $uri = $_SERVER['REQUEST_URI'];
-        $pattern = self::pathToRegex($path);
-        self::setMeta($staticMeta);
-
-        $uri = str_replace('frame/', '', $uri);
-
-        if (preg_match($pattern, $uri, $matches)) {
-            array_shift($matches);
-
-            $frameUrl = $uri . '/frame/';
-            $frameUrl = str_replace('//', '/', $frameUrl);
-
-            if ($GLOBALS['framemode'] == 0) {
-                $GLOBALS['data'] = '<div id="spectro_block5"><center><img src="{SITE}spectro-cms-loading.gif" height="50"></center><input id="spectro_dynamicblock5" type="hidden" value="' . $frameUrl .'"></div>';
-                $GLOBALS['dosomething'] = 1;
-            } else {
-                
-                self::page($path, $callback, $staticMeta);
-            }
-        }
+        self::$routes[$path] = [
+            'path' => $path,
+            'callback' => $callback,
+            'staticMeta' => $staticMeta,
+            'type' => 'lazy'
+        ];
     }
 
     public static function json($path, $callback) {
-        self::page($path, $callback, ['json' => true]);
+        self::$routes[$path] = [
+            'path' => $path,
+            'callback' => $callback,
+            'type' => 'json'
+        ];
     }
 
     public static function useTemplate($template) {
