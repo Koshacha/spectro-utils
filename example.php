b2a271c96aac3e86acc7b9c27b6f89cef9cff667
Ilya M. <qbub@proton.me>
2025-08-16 23:35:47 +0700

    feat: Implement Entity, DB, Updater classes and install script
    - Add Entity class for dynamic entity creation and management.
    - Add Db class as a static database wrapper.
    - Enhance Updater class with safer updates, rollbacks, and update checks.
    - Create install.php for initial installation from the repository.

--- example.php
+++ example.php
@@ -25,3 +25,65 @@ Route::lazyPage('/about/:cat/:id', function($id, $cat, $options) {
         'DESCRIPTION' => 'About Us ' . $id
     ];
 });
+
+Entity::register('metal', [
+    'NAME' => 'name',
+    'S01' => ['sex', 'birthday', 'phone']
+    'S02' => ['phrase', 'registerDate'],
+    [
+        'age' => function () {
+            return date('Y') - $this->birthday;
+        },
+        'gender' => function () {
+            return $this->age['sex'] === 'male' ? 'муж' : 'жен';
+        }
+    ]
+], [
+    'table' => PREFIX . "ROWS",
+]);
+
+MetalEntity::create([
+    'name' => 'Железо',
+    'sex' => 'male',
+    'birthday' => '2000-01-01',
+    'phone' => '123456789',
+    'phrase' => 'Железо!!',
+    'registerDate' => '2025-01-01'
+]);
+
+$jelezo = MetalEntity::getOne(1); // by id
+
+// OR
+
+$jelezo = MetalEntity::getOne([
+    'or',
+    ['name', '=', 'Железо'],
+    ['id', '<', 10]
+]);
+
+MetalEntity::update(1 /* id */, [
+    'name' => 'Железо',
+]);
+
+// OR
+
+MetalEntity::updateAll([
+    ['id', '<', 10]
+], [
+    'name' => 'Железо',
+    'sex' => function () {
+        return $this->sex === 'male' ? 'female' : 'male';
+    }
+]);
+
+$allJelezo = MetalEntity::get([
+    'or',
+    ['name', '=', 'Железо'],
+    ['id', '<', 10]
+]); // [ [...], ... ]
+
+/*
+$jelezo === ['id' => 1, 'name' => 'Железо', 'sex' => 'male', 'birthday' => '2000-01-01', 'phone' => '123456789', 'phrase' => 'Железо!!', 'registerDate' => '2025-01-01', 'age' => 25, 'gender' => 'муж', 'createdAt' => '2020-01-01 00:00:00']
+*/
+
+MetalEntity::deleteById(1);
